fa321fda563a3b6b077d4bdeea95a84556659f05
VijayMishra-PC\Vijay Mishra <sud9458@gmail.com>
2017-03-19 01:56:39 +0530

    space deleted

--- New Project-20170312/HelloWorld.java
+++ New Project-20170312/HelloWorld.java
@@ -9,11 +9,11 @@ public void run(){
      public static void main(String []args){
         System.out.println("Hello World");
         HelloWorld m1=new HelloWorld();  
-  HelloWorld m2=new HelloWorld();  
-  m1.setPriority(Thread.MIN_PRIORITY);  
-  m1.yield();
-  m2.setPriority(Thread.MAX_PRIORITY);  
-  m1.start();  
-  m2.start();  
+  	HelloWorld m2=new HelloWorld();  
+  	m1.setPriority(Thread.MIN_PRIORITY);  
+  	m1.yield();
+  	m2.setPriority(Thread.MAX_PRIORITY);  
+  	m1.start();  
+  	m2.start();  
      }
 }
