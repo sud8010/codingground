2e717ae0b5a7dfe7c053522e83bc96eef49df869
VijayMishra-PC\Vijay Mishra <sud9458@gmail.com>
2017-03-19 21:07:49 +0530

    space deleted

--- New Project-20170312/HelloWorld.java
+++ New Project-20170312/HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld extends Thread{
 
 public void run(){  
-   System.out.println("running thread name is:"+Thread.currentThread().getName());  
+   
+System.out.println("running thread name is:"+Thread.currentThread().getName());  
    System.out.println("running thread priority is:"+Thread.currentThread().getPriority());    
   }  
 
